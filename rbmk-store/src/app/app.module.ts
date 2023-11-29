9b71745e43af6d8b14d2b76e748dd89310303315
fernandoted <fernando.lucas@yaman.com.br>
2023-11-29 04:00:08 -0300

    creating card-item component

--- rbmk-store/src/app/app.module.ts
+++ rbmk-store/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreSectionComponent } from './store-section/store-section.component';
+import { CardItemComponent } from './components/card-item/card-item.component';
 
 @NgModule({
     declarations: [
@@ -16,7 +17,8 @@ import { StoreSectionComponent } from './store-section/store-section.component';
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
-        StoreSectionComponent
+        StoreSectionComponent,
+        CardItemComponent
     ]
 })
 export class AppModule { }
--- rbmk-store/src/app/components/card-item/card-item.component.ts
+++ rbmk-store/src/app/components/card-item/card-item.component.ts
@@ -4,21 +4,19 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
-import { CardItemComponent } from '../components/card-item/card-item.component';
 
 @Component({
-  selector: 'app-store-section',
-  templateUrl: './store-section.component.html',
-  styleUrls: ['./store-section.component.scss'],
+  selector: 'app-card-item',
+  templateUrl: './card-item.component.html',
+  styleUrls: ['./card-item.component.scss'],
   standalone: true,
   imports: [
     MatCardModule,
     MatButtonModule,
     MatIconModule,
-    MatDividerModule,
-    CardItemComponent
+    MatDividerModule
   ],
 })
-export class StoreSectionComponent {
+export class CardItemComponent {
 
 }
